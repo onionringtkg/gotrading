3e21de731eb38e7286c20ea292fadeef548918b3
Okano Takumi <oknokn.of.12@gmail.com>
2020-04-14 18:09:52 +0900

    upload golang app practice

---
+++ app/models/events.go
@@ -0,0 +1,203 @@
+package models
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"gotrading/config"
+)
+
+type SignalEvent struct {
+	Time        time.Time `json:"time"`
+	ProductCode string    `json:"product_code"`
+	Side        string    `json:"side"`
+	Price       float64   `json:"price"`
+	Size        float64   `json:"size"`
+}
+
+func (s *SignalEvent) Save() bool {
+	cmd := fmt.Sprintf("INSERT INTO %s (time, product_code, side, price, size) VALUES (?, ?, ?, ?, ?)", tableNameSignalEvents)
+	_, err := DbConnection.Exec(cmd, s.Time.Format(time.RFC3339), s.ProductCode, s.Side, s.Price, s.Size)
+	if err != nil {
+		if strings.Contains(err.Error(), "UNIQUE constraint failed") {
+			log.Println(err)
+			return true
+		}
+		return false
+	}
+	return true
+}
+
+//BUY SELL SELL BUY のように管理していく
+type SignalEvents struct {
+	Signals []SignalEvent `json:"signals,omitempty"`
+}
+
+func NewSignalEvents() *SignalEvents {
+	return &SignalEvents{}
+}
+
+func GetSignalEventsByCount(loadEvents int) *SignalEvents {
+	cmd := fmt.Sprintf(`SELECT * FROM (
+        SELECT time, product_code, side, price, size FROM %s WHERE product_code = ? ORDER BY time DESC LIMIT ? )
+        ORDER BY time ASC;`, tableNameSignalEvents)
+	rows, err := DbConnection.Query(cmd, config.Config.ProductCode, loadEvents)
+	if err != nil {
+		return nil
+	}
+	defer rows.Close()
+
+	var signalEvents SignalEvents
+	for rows.Next() {
+		var signalEvent SignalEvent
+		rows.Scan(&signalEvent.Time, &signalEvent.ProductCode, &signalEvent.Side, &signalEvent.Price, &signalEvent.Size)
+		signalEvents.Signals = append(signalEvents.Signals, signalEvent)
+	}
+	err = rows.Err()
+	if err != nil {
+		return nil
+	}
+	return &signalEvents
+}
+
+func GetSignalEventsAfterTime(timeTime time.Time) *SignalEvents {
+	cmd := fmt.Sprintf(`SELECT * FROM (
+                SELECT time, product_code, side, price, size FROM %s
+                WHERE DATETIME(time) >= DATETIME(?)
+                ORDER BY time DESC
+            ) ORDER BY time ASC;`, tableNameSignalEvents)
+	rows, err := DbConnection.Query(cmd, timeTime.Format(time.RFC3339))
+	if err != nil {
+		return nil
+	}
+	defer rows.Close()
+
+	var signalEvents SignalEvents
+	for rows.Next() {
+		var signalEvent SignalEvent
+		rows.Scan(&signalEvent.Time, &signalEvent.ProductCode, &signalEvent.Side, &signalEvent.Price, &signalEvent.Size)
+		signalEvents.Signals = append(signalEvents.Signals, signalEvent)
+	}
+	return &signalEvents
+}
+
+//購入可否の判断
+func (s *SignalEvents) CanBuy(time time.Time) bool {
+	lenSignals := len(s.Signals)
+	if lenSignals == 0 {
+		return true
+	}
+
+	lastSignal := s.Signals[lenSignals-1]
+	if lastSignal.Side == "SELL" && lastSignal.Time.Before(time) {
+		return true
+	}
+	return false
+}
+
+func (s *SignalEvents) CanSell(time time.Time) bool {
+	lenSignals := len(s.Signals)
+	if lenSignals == 0 {
+		return false
+	}
+
+	lastSignal := s.Signals[lenSignals-1]
+	if lastSignal.Side == "BUY" && lastSignal.Time.Before(time) {
+		return true
+	}
+	return false
+}
+
+//購入処理
+func (s *SignalEvents) Buy(ProductCode string, time time.Time, price, size float64, save bool) bool {
+	if !s.CanBuy(time) {
+		return false
+	}
+	signalEvent := SignalEvent{
+		ProductCode: ProductCode,
+		Time:        time,
+		Side:        "BUY",
+		Price:       price,
+		Size:        size,
+	}
+	if save {
+		signalEvent.Save()
+	}
+	s.Signals = append(s.Signals, signalEvent)
+	return true
+}
+
+//売却
+func (s *SignalEvents) Sell(productCode string, time time.Time, price, size float64, save bool) bool {
+
+	if !s.CanSell(time) {
+		return false
+	}
+
+	signalEvent := SignalEvent{
+		ProductCode: productCode,
+		Time:        time,
+		Side:        "SELL",
+		Price:       price,
+		Size:        size,
+	}
+	if save {
+		signalEvent.Save()
+	}
+	s.Signals = append(s.Signals, signalEvent)
+	return true
+}
+
+//終始計算
+func (s *SignalEvents) Profit() float64 {
+	total := 0.0
+	beforeSell := 0.0
+	isHolding := false
+	for i, signalEvent := range s.Signals {
+		if i == 0 && signalEvent.Side == "SELL" {
+			continue
+		}
+		if signalEvent.Side == "BUY" {
+			total -= signalEvent.Price * signalEvent.Size
+			isHolding = true
+		}
+		if signalEvent.Side == "SELL" {
+			total += signalEvent.Price * signalEvent.Size
+			isHolding = false
+			beforeSell = total
+		}
+	}
+	//計算処理が完了するまでは購入できない
+	if isHolding == true {
+		return beforeSell
+	}
+	return total
+}
+
+//JSON形式の指定
+func (s SignalEvents) MarshalJSON() ([]byte, error) {
+	value, err := json.Marshal(&struct {
+		Signals []SignalEvent `json:"signals,omitempty"`
+		Profit  float64       `json:"profit,omitempty"`
+	}{
+		Signals: s.Signals,
+		Profit:  s.Profit(),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return value, err
+}
+
+func (s *SignalEvents) CollectAfter(time time.Time) *SignalEvents {
+	for i, signal := range s.Signals {
+		if time.After(signal.Time) {
+			continue
+		}
+		return &SignalEvents{Signals: s.Signals[i:]}
+	}
+	return nil
+}
